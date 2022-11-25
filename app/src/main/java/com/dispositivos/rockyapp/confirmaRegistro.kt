1b81433e0676280a739a9704b5486026562ddff4
AndresAMedina <ingandresmedina@gmail.com>
2022-11-25 08:25:36 -0500

    final andres login y resiclerview

--- app/src/main/java/com/dispositivos/rockyapp/confirmaRegistro.kt
+++ app/src/main/java/com/dispositivos/rockyapp/confirmaRegistro.kt
@@ -5,29 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class RegisterActivity3 : AppCompatActivity() {
+class confirmaRegistro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register3)
+        setContentView(R.layout.activity_confirma_registro)
 
-        val btn: Button = findViewById(R.id.btn_AtrasRegister)
+        val btn: Button = findViewById(R.id.btn_okReg)
         btn.setOnClickListener {
 
             val intent = Intent(this, LoginActivity2::class.java)
             startActivity(intent)
 
         }
-        val btn1: Button = findViewById(R.id.btn_registro)
-        btn1.setOnClickListener {
-
-            val intent = Intent(this, confirmaRegistro::class.java)
-            startActivity(intent)
-
-        }
-
-
-
-
 
     }
 }
--- app/src/main/java/com/dispositivos/rockyapp/perroPro.kt
+++ app/src/main/java/com/dispositivos/rockyapp/perroPro.kt
@@ -7,51 +7,72 @@ class perroPro {
         val perroList = listOf<perro>(
 
             perro(
-                "Consejos",
-                "Cuidados",
-                "Perro y Gato",
-                "https://images.pexels.com/photos/2795879/pexels-photo-2795879.jpeg"
+                "PETPLUS",
+
+                        " \nTelefonos:\n" +
+                        "Tel: 695 7656 – 715-6389\n" +
+                        "Cel: 3502837021",
+                " Veterinaria para Perros y Gatos",
+                "https://www.petplusclinica.com/wp-content/uploads/2022/10/HOSPITALweb.jpg.webp"
             ),
             perro(
-                "Vacunas",
-                "Cuidados",
-                "Perro y Gato",
-                "https://images.pexels.com/photos/7474852/pexels-photo-7474852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1 "
+                "Spa Club Pets",
+                " Llamanos ahora al +573053909011",
+                " Peluqueria para Perros y Gatos",
+                "https://images.pexels.com/photos/7788609/pexels-photo-7788609.jpeg"
             ),
             perro(
-                "Aseo",
-                "Cuidados",
-                "Perro y Gato",
+                "Parque Para Trotar Con Perros",
+                "Dirección: Cl. 40 Sur #22a-09, Bogotá",
+                "Parque para Perros y Gatos",
+                "https://images.pexels.com/photos/4555468/pexels-photo-4555468.jpeg"
+            ),
+            perro(
+                "Parque de Los Perros",
+                "Dirección: Cra. 9 #93-98, Bogotá",
+                "Parque para Perros y Gatos",
+                "https://images.pexels.com/photos/416204/pexels-photo-416204.jpeg"
+            ),
+            perro(
+                "PETCOL",
+                "Call center\n" +
+                        "3222890352\n" +
+                        "\n" +
+                        "Cedritos: 3222890352\n" +
+                        "Batan: 3014357609\n" +
+                        "Villas: 3175008496\n" +
+                        "Rosales: 3012860096\n" +
+                        "Pontevedra: 3003165160\n",
+                "Peluqueria para Perros y Gatos",
                 "https://images.pexels.com/photos/6131156/pexels-photo-6131156.jpeg"
             ),
             perro(
-                "Actividades",
-                "Juegos",
-                "Perro y Gato",
-                "https://images.pexels.com/photos/3857521/pexels-photo-3857521.jpeg"
-            ),
-            perro(
-                "Juguetes",
-                "Juegos",
-                "Perro y Gato",
-                "https://images.pexels.com/photos/6131156/pexels-photo-6131156.jpeg"
-            ),
-            perro(
-                "Centro de adopcion",
-                "Ayuda a un peludo",
-                "Perro y Gato",
+                "Gatos y Blues",
+                "Calle 42 No.22-54, Bogotá 111311, Colombia",
+                "El mejor sitio para los amantes de los gatos",
                 "https://images.pexels.com/photos/14440674/pexels-photo-14440674.jpeg"
             ),
             perro(
                 "Donaciones",
-                "Ayuda a un peludo",
+                "Las donaciones, que incluyen:\n"+
+                        "alimentos, juguetes, cobijas, \n"+
+                        "camas, elementos de aseo y \n"+
+                        "medicamentos, podrán ser entregadas\n"+
+                        "de lunes a viernes de 8:00 a.m. a \n"+
+                        "5:00 p.m. en las Alcaldías locales \n"+
+                        "o en las instalaciones del Instituto\n"+
+                        "de Protección y Bienestar Animal, \n"+
+                        "ubicada en la Calle 116 #70g – 82 \n",
                 "Perro y Gato",
                 "https://images.pexels.com/photos/6591164/pexels-photo-6591164.jpeg"
             ),
             perro(
                 "Policia de rescate animal",
-                "Ayuda a un peludo",
-                "Perro y Gato",
+                "Atención administrativa:\n" +
+                        "Carrera 59 26-21 CAN, Bogotá \n" +
+                        "Lunes a viernes de 08:00 a.m – 12:00 p.m y 02:00 p.m – 05:00 p.m\n" +
+                        "Conmutador o PBX: (601) 5159000",
+                "Ayuda a los Perros y Gatos",
                 "https://images.pexels.com/photos/9428919/pexels-photo-9428919.jpeg"
             )
 
